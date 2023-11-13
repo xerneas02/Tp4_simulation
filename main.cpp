e72af827262405334da24daa2871734ad58046d2
xerneas02 <mathis.roubille@gmail.com>
2023-11-13 15:26:47 +0100

    Plus d'affichage et de stat OoO

--- main.cpp
+++ main.cpp
@@ -14,20 +14,38 @@ int main()
     long double total = 0;
     int nbSimu = 1000;
 
+    long double totalPerMonth[NUMBER_OF_YEAR*MONTH_PER_YEAR] = {0};
+    long double totalPerCategory[MAX_CATEGORY] = {0};
+
     for (int j = 0; j < nbSimu; j++)
     {
         printf("%d/%d\n", j, nbSimu);
         for (int i = 0; i < NUMBER_OF_YEAR*MONTH_PER_YEAR; i++)
         {
-            //printf("Month : %d - %llu\n", i, sim->getNbRabbits());
+            totalPerMonth[i] += (long double)sim->getNbRabbits()/nbSimu;
             sim->nextMonth();        
         }
+
+        for (int i = 0; i < MAX_CATEGORY; i++)
+        {
+            totalPerCategory[i] += (long double)sim->getCategory(i)->getNbRabbits()/nbSimu;
+        }
+        
         total += (long double)sim->getNbRabbits()/nbSimu;
 
         sim->reset();
     }
 
     printf("Nombre de lapin moyen apres %dans : %0.Lf (%0.LfM)\n", NUMBER_OF_YEAR, total, total/1000000000);
+    for (int i = 0; i < NUMBER_OF_YEAR*MONTH_PER_YEAR; i++)
+    {
+        printf("Month : %d - %Lf\n", i, totalPerMonth[i]);       
+    }
+
+    for (int i = 0; i < MAX_CATEGORY; i++)
+    {
+        printf("Category: %d - %Lf\n", i, totalPerCategory[i]);       
+    }
 
     
     
