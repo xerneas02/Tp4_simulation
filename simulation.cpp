1defbbb64a7b9eefeb877c7bb48bfb9f89992501
xerneas02 <mathis.roubille@gmail.com>
2023-11-17 18:14:49 +0100

    docstring simulation

--- simulation.cpp
+++ simulation.cpp
@@ -3,6 +3,12 @@
 #include  "mt.hpp"
 #include <math.h>
 
+/**
+ * @brief Constructeur de la classe Simulation.
+ *
+ * @param nMales Le nombre initial de lapins mâles.
+ * @param nFemales Le nombre initial de lapins femelles.
+ */
 Simulation::Simulation(ull nMales, ull nFemales)
 {
     for (ull i = 0; i < MAX_CATEGORY; i++)
@@ -19,12 +25,14 @@ Simulation::Simulation(ull nMales, ull nFemales)
     }
 
     maleStart = nMales;
-    femaleStart = nFemales;
-
-    ecartType = 20;
-        
+    femaleStart = nFemales;   
 }
 
+/**
+ * @brief Réinitialise la simulation.
+ *
+ * Cette fonction réinitialise tous les paramètres de la simulation à leurs valeurs initiales.
+ */
 void Simulation::reset()
 {
     for (ull i = 0; i < MAX_CATEGORY; i++)
@@ -41,7 +49,11 @@ void Simulation::reset()
     }
 }
 
-
+/**
+ * @brief Passe au mois suivant dans la simulation.
+ *
+ * Cette fonction simule l'évolution de la population de lapins d'un mois à l'autre.
+ */
 void Simulation::nextMonth()
 {
     if (month == 0)
@@ -60,6 +72,15 @@ void Simulation::nextMonth()
     month = (month + 1) % MONTH_PER_YEAR;
 }
 
+/**
+ * @brief Obtient le nombre de couples de lapins.
+ * 
+ * Cette fonction considère qu'il faut au moins 1 mâle pour 10 femelles.
+ * Si le nombre de mâles est 10 fois moins que le nombre de femelles, la fonction retourne
+ * 10 fois le nombre de mâles; sinon, elle retourne le nombre de femelles.
+ *
+ * @return Le nombre de couples.
+ */
 ull Simulation::getNbCouples()
 {
     ull females = 0;
@@ -74,11 +95,22 @@ ull Simulation::getNbCouples()
     return females < males ? females : males;
 }
 
+ /**
+ * @brief Obtient un pointeur vers la catégorie de lapins à l'indice spécifié.
+ *
+ * @param i L'indice de la catégorie de lapins à récupérer (le mois).
+ * @return Un pointeur vers la catégorie de lapins à l'indice spécifié.
+ */
 RabbitCategory * Simulation::getCategory(ull i)
 {
     return categories[i];
 }
 
+/**
+ * @brief Obtient le nombre total de lapins dans la population.
+ *
+ * @return Le nombre total de lapins.
+ */
 ull Simulation::getNbRabbits()
 {
     ull rabbits = 0;
@@ -91,12 +123,22 @@ ull Simulation::getNbRabbits()
     return rabbits;
 }
 
+/**
+ * @brief Génère un nombre aléatoire de bébés entre 3 et 6.
+ *
+ * @return Le nombre de bébés.
+ */
 ull Simulation::genRandBabys()
 {
     return rand_int_uniform(3, 7);
 
 }
 
+/**
+ * @brief Génère un nombre aléatoire de portées entre 4 et 8, avec une plus grande probabilité d'avoir 5, 6 ou 7.
+ *
+ * @return Le nombre de portées.
+ */
 ull Simulation::genRandLitters()
 {
     double r = genrand_real1();
@@ -106,6 +148,9 @@ ull Simulation::genRandLitters()
     return 5 + (r-0.25)/0.25;
 }
 
+/**
+ * @brief Génère le nombre total de bébés pour l'année suivante et les répartit pour chaque mois.
+ */
 void Simulation::howManyBabys()
 {
     ull couples = getNbCouples();
@@ -121,9 +166,7 @@ void Simulation::howManyBabys()
     else 
     {
         litters = genererGaussienne(couples*6, 380);
-    }
-    
-        
+    }    
 
     ull littersPerMonth[MONTH_PER_YEAR];
     for (ull i = 0; i < MONTH_PER_YEAR; i++){
--- simulation.hpp
+++ simulation.hpp
@@ -4,38 +4,99 @@
 #define MAX_CATEGORY 20*12
 #define START_AGE 12
 
-
-
-
 #include "rabbitCategory.hpp"
 
+/**
+ * @class Simulation
+ * @brief Classe représentant une simulation de population de lapins.
+ *
+ * La classe Simulation gère la simulation de l'évolution d'une population de lapins
+ * en fonction de différentes catégories d'âge et de genres. Elle maintient des informations
+ * telles que le nombre de lapins mâles et femelles pour chaque mois, le nombre total de lapins,
+ * l'écart type, etc.
+ */
 class Simulation
 {
 private:
-    ull maleNextYear  [MONTH_PER_YEAR];
-    ull femaleNextYear[MONTH_PER_YEAR];
-    ull nbYears; // simulation's number of years
-    ull month; // current month
-    ull nbRabbits; 
-    double ecartType;
+    ull maleNextYear[MONTH_PER_YEAR];        // Nombre de lapins mâles pour l'année suivante (par mois)
+    ull femaleNextYear[MONTH_PER_YEAR];      // Nombre de lapins femelles pour l'année suivante (par mois)
+    ull nbYears;                             // Nombre d'années de simulation
+    ull month;                               // Mois actuel
+    ull nbRabbits;                           // Nombre total de lapins
 
-    ull maleStart;
-    ull femaleStart;
+    ull maleStart;                           // Nombre initial de lapins mâles
+    ull femaleStart;                         // Nombre initial de lapins femelles
 
-    RabbitCategory * categories[MAX_CATEGORY]; // rabbits categories
+    RabbitCategory *categories[MAX_CATEGORY]; // Tableau de pointeurs vers des objets RabbitCategory
 
-    ull  getNbCouples();
-    ull  genRandBabys();
+    /**
+     * @brief Obtient le nombre de couples de lapins.
+     * 
+     * Cette fonction considère qu'il faut au moins 1 mâle pour 10 femelles.
+     * Si le nombre de mâles est 10 fois moins que le nombre de femelles, la fonction retourne
+     * 10 fois le nombre de mâles; sinon, elle retourne le nombre de femelles.
+     *
+     * @return Le nombre de couples.
+     */
+    ull getNbCouples();
+
+    /**
+     * @brief Génère un nombre aléatoire de bébés entre 3 et 6.
+     *
+     * @return Le nombre de bébés.
+     */
+    ull genRandBabys();
+
+    /**
+     * @brief Génère un nombre aléatoire de portées entre 4 et 8, avec une plus grande probabilité d'avoir 5, 6 ou 7.
+     *
+     * @return Le nombre de portées.
+     */
+    ull genRandLitters();
+
+    /**
+     * @brief Génère le nombre total de bébés pour l'année suivante et les répartit pour chaque mois.
+     */
     void howManyBabys();
-    ull  genRandLitters();
-    
 
 public:
+    /**
+     * @brief Constructeur de la classe Simulation.
+     *
+     * @param nMales Le nombre initial de lapins mâles.
+     * @param nFemales Le nombre initial de lapins femelles.
+     */
     Simulation(ull nMales, ull nFemales);
+
+    /**
+     * @brief Réinitialise la simulation.
+     *
+     * Cette fonction réinitialise tous les paramètres de la simulation à leurs valeurs initiales.
+     */
     void reset();
+
+    /**
+     * @brief Passe au mois suivant dans la simulation.
+     *
+     * Cette fonction simule l'évolution de la population de lapins d'un mois à l'autre.
+     */
     void nextMonth();
-    ull  getNbRabbits();
-    RabbitCategory * getCategory(ull i);
+
+    /**
+     * @brief Obtient le nombre total de lapins dans la population.
+     *
+     * @return Le nombre total de lapins.
+     */
+    ull getNbRabbits();
+
+    /**
+     * @brief Obtient un pointeur vers la catégorie de lapins à l'indice spécifié.
+     *
+     * @param i L'indice de la catégorie de lapins à récupérer (le mois).
+     * @return Un pointeur vers la catégorie de lapins à l'indice spécifié.
+     */
+    RabbitCategory *getCategory(ull i);
 };
+
 
 #endif
